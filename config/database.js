cf9e4cefce213db417acb5f36d91a92693d8b8db
lexiedooley <lexiedools@gmail.com>
2023-06-22 19:28:40 -0400

    app crashed, fixed files, app no longer crashing

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_IRL);
+mongoose.connect(process.env.DATABASE_URL);
 
 // mongoose.connection object
 const db = mongoose.connection;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
-require('.config/database')
+require('./config/database')
 
 var indexRouter = require('./routes/index');
 var ghostsRouter = require('./routes/ghosts');
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride("_method"))
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/ghosts', ghostsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
