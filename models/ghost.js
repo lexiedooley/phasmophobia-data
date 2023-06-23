9bd978506947e5260fdab33d232b19c6ae104956
lexiedooley <lexiedools@gmail.com>
2023-06-23 15:26:53 -0400

    added header/footer, added mapSchema

--- models/ghost.js
+++ models/ghost.js
@@ -4,9 +4,21 @@ const Schema = mongoose.Schema;
 
 const ghostSchema = new Schema (
 {
-    name: 'String',
-    occurrances: Number
+    ghostName: 'String',
+    ghostOccurrances: Number
 })
 
+const mapSchema = new Schema 
+{
+    mapName: 'String',
+    rooms = [
+    {    
+        roomName: 'String',
+        roomOccurrances: Number
+    }
+    ]     
+}
+
 module.exports = mongoose.model('Ghost', ghostSchema)
+module.exports = mongoose.model('Map', mapSchema)
 
